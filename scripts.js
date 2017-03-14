b6e68099521c3378b4000ae5ae5efbcf72a494cf
willikers19 <wtk219@nyu.edu>
2017-03-14 12:12:28 -0400

    update

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/*
+<script src="http://www.google.com/jsapi" type="text/javascript"></script>
+<script type="text/javascript">google.load("jquery", "1.3.2");</script>
+<script type="text/javascript" src="scripts.js"/>
+*/
+
+$(document).ready(function() {
+
+});
